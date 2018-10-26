cd9398e8830baf99145335728ef6aa268cd89170
Chan Lee <chanethanlee@gmail.com>
2018-10-26 12:18:51 -0700

    add navmenu model and apply to navbar for loop.

---
+++ src/app/model/nav-menu.ts
@@ -0,0 +1,9 @@
+export class NavMenu {
+    constructor (public name?: string, public list?: string[]) {}
+}
+
+// export class NavItem {
+//     constructor(public body?:string, public isBold?: boolean) {
+
+//     }
+// }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { AuthService } from './../services/auth.service';
+import { NavMenu } from '../model/nav-menu';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,4 +10,9 @@ import { AuthService } from './../services/auth.service';
 export class NavBarComponent {
 
   constructor(private authService: AuthService) { }
+  listStringOne = "New Arrivals Jeffrey's Edit Pop-In: Memphis Milano Designer Brands Balenciaga Burberry CALVIN KLEIN 205W39NYC Chloé Christian Louboutin Dries Van Noten Fendi Givenchy Gucci Loewe Prada Saint Laurent Valentino All Women's Designer Brands All Men's Designer Brands Women Designer Clothing signer Shoes Designer Handbags Designer Jewelry Designer Accessories Men All Men's Designer Designer Clothing Designer Shoes Designer Accessories Kids Designer Girls Designer Boys Designer Baby Girl Designer Baby Boy Designer Shoes Designer Accessories SPACE: Emerging & Advanced Designer Clothing Shoes Handbags Jewelry Accessories Home SPACE: Emerging & Advanced Designer Brands Acne Studios Comme des Garçons Jacquemus Off-White Simon Miller Simone Rocha Vetements WWAKE";
+
+  navMenuOne= new NavMenu("Designer", this.listStringOne.split[" "]);
+
+  navMenus = [this.navMenuOne];
 }
