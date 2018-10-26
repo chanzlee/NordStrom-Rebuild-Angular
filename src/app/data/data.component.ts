38ec1789321d7c9765d0411b8fdc39e5fb90d3a8
Chan Lee <chanethanlee@gmail.com>
2018-10-26 16:21:29 -0700

    change table layout into div rows and cols.

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -12,4 +12,7 @@ export class DataComponent{
 
   row1col2 = new Data("Upload Your Selfie","Snap a pic and we'll create your custom Nordy portrait—because we're all about personalizing your rewards. Download the Nordstrom app to get started.", "Download the App", "https://shop.nordstrom.com/content/nordstrom-apps?cm_sp=merch-_-corp_6749_j009165-_-hp_corp_p00_info&jid=j009165-6749")
 
+  row2col1 = new Data("New & Only Here","The East Coast-meets-West Coast vibe of mega influencer Blair Eadie plus the modern ease and versatility of Halogen—select styles up to size 24. This collection is going to transform your closet.","Shop Now", "https://shop.nordstrom.com/c/halogen-atlantic-pacific-all?campaign=1022halogenaphpa&jid=j009296-5965&cid=wiif4&cm_sp=merch-_-npg_5965_j009296-_-mowhp_corp_p01_shop")
+
+
 }
