deaae181e9cab27315caf72203f4b9902db6ed50
Chan Lee <chanethanlee@gmail.com>
2018-10-26 10:06:09 -0700

    add basic top component structure.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private authService: AuthService) { }
 }
